b14d40e32f0d4659e5891001b612aa30a6f9369c
hothuuk <kijkj123@gmail.com>
2024-06-03 18:12:26 +0900

    initial commit

---
+++ src/main/java/htmake/jtbot/JtbotApplication.java
@@ -0,0 +1,13 @@
+package htmake.jtbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtbotApplication.class, args);
+	}
+
+}
