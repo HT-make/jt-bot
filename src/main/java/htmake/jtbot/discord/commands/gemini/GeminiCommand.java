e22210ca951dc469f1e654390002cdd471ab4462
오진서 <s22030@gsm.hs.kr>
2024-06-07 23:15:53 +0900

    update :: del validCheck

--- src/main/java/htmake/jtbot/discord/commands/gemini/GeminiCommand.java
+++ src/main/java/htmake/jtbot/discord/commands/gemini/GeminiCommand.java
@@ -1,8 +1,6 @@
 package htmake.jtbot.discord.commands.gemini;
 
 import htmake.jtbot.discord.commands.gemini.event.GeminiChatSlashCommand;
-import htmake.jtbot.discord.util.ErrorUtil;
-import htmake.jtbot.discord.util.MessageUtil;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -12,14 +10,9 @@ import org.springframework.stereotype.Component;
 public class GeminiCommand extends ListenerAdapter {
 
     private final GeminiChatSlashCommand geminiChatSlashCommand;
-    private final ErrorUtil errorUtil;
-    private final MessageUtil messageUtil;
 
     public GeminiCommand() {
         this.geminiChatSlashCommand = new GeminiChatSlashCommand();
-
-        this.errorUtil = new ErrorUtil();
-        this.messageUtil = new MessageUtil();
     }
 
     @Override
@@ -27,11 +20,6 @@ public class GeminiCommand extends ListenerAdapter {
         String command = event.getName();
 
         if (command.equals("호떡아")) {
-            if (messageUtil.validCheck(event.getUser().getId())) {
-                errorUtil.sendError(event, "작업 실패", "현재 다른 작업을 진행중입니다.");
-                return;
-            }
-
             geminiChatSlashCommand.execute(event);
         }
     }
