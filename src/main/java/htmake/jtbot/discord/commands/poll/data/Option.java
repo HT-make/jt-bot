8390f5faff6aea994901c1d2c8305fafb41b0ae9
hothuuk <kijkj123@gmail.com>
2024-06-08 22:31:55 +0900

    create :: Option

---
+++ src/main/java/htmake/jtbot/discord/commands/poll/data/Option.java
@@ -0,0 +1,19 @@
+package htmake.jtbot.discord.commands.poll.data;
+
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Option {
+
+    private String title;
+
+    private int votes;
+
+    private Set<String> votingUser;
+}
