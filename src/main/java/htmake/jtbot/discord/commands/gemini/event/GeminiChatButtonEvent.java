3aac576b0ae9bd8f954d0f92fe3f58e7c885f1c4
오진서 <s22030@gsm.hs.kr>
2024-06-11 10:58:26 +0900

    create :: Gemini chat button event

---
+++ src/main/java/htmake/jtbot/discord/commands/gemini/event/GeminiChatButtonEvent.java
@@ -0,0 +1,86 @@
+package htmake.jtbot.discord.commands.gemini.event;
+
+import htmake.jtbot.discord.commands.gemini.cache.GeminiChatCache;
+import htmake.jtbot.discord.commands.gemini.data.Chat;
+import htmake.jtbot.discord.util.ErrorUtil;
+import htmake.jtbot.global.cache.CacheFactory;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class GeminiChatButtonEvent {
+
+    private final ErrorUtil errorUtil;
+
+    private final GeminiChatCache geminiChatCache;
+
+    public GeminiChatButtonEvent() {
+        this.errorUtil = new ErrorUtil();
+
+        this.geminiChatCache = CacheFactory.geminiChatCache;
+    }
+
+    public void execute(ButtonInteractionEvent event, int chatId, int page) {
+        Optional<Chat> optionalChat = Optional.ofNullable(geminiChatCache.get(chatId));
+        if (optionalChat.isPresent()) {
+            Chat chat = optionalChat.get();
+            MessageEmbed newEmbed = buildEmbed(page, chat);
+
+            int maxPage = chat.getMaxPage();
+            List<Button> buttonList = buttonEmbed(page, maxPage, chatId);
+
+            event.getHook().editOriginalEmbeds(newEmbed)
+                    .setActionRow(buttonList)
+                    .queue();
+        } else {
+            errorUtil.sendError(event.getHook(), "호떡이", "대화 내용을 찾을 수 없습니다.");
+        }
+    }
+
+    private MessageEmbed buildEmbed(int page, Chat chat) {
+        List<String> answerList = chat.getAnswerList();
+        String question = chat.getQuestion();
+
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setColor(Color.GREEN);
+
+        if (page == 1) {
+            embedBuilder.addField(question, "", false);
+            embedBuilder.addField("", answerList.get(0), false);
+        } else if (page <= answerList.size()) {
+            embedBuilder.addField("", answerList.get(page - 1), false);
+        }
+
+        return embedBuilder.build();
+    }
+
+    public List<Button> buttonEmbed(int page, int maxPage, int chatId) {
+        List<Button> buttonList = new ArrayList<>();
+
+        Button leftButton = Button.primary("gemini-chat-" + chatId + "-" + (page - 1), "◄");
+        Button pageButton = Button.secondary("blank", page + "/" + maxPage).asDisabled();
+        Button rightButton = Button.primary("gemini-chat-" + chatId + "-" + (page + 1), "►");
+
+        if (page == 1) {
+            buttonList.add(leftButton.asDisabled());
+            buttonList.add(pageButton);
+            buttonList.add(rightButton);
+        } else if (page == maxPage) {
+            buttonList.add(leftButton);
+            buttonList.add(pageButton);
+            buttonList.add(rightButton.asDisabled());
+        } else {
+            buttonList.add(leftButton.asEnabled());
+            buttonList.add(pageButton);
+            buttonList.add(rightButton.asEnabled());
+        }
+
+        return buttonList;
+    }
+}
