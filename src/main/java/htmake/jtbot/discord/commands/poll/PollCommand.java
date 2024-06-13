3269cd9238518d81e0a3515a0cd53cbc77a16bb0
hothuuk <kijkj123@gmail.com>
2024-06-13 20:38:39 +0900

    add :: Added addOptionEvents

--- src/main/java/htmake/jtbot/discord/commands/poll/PollCommand.java
+++ src/main/java/htmake/jtbot/discord/commands/poll/PollCommand.java
@@ -1,7 +1,10 @@
 package htmake.jtbot.discord.commands.poll;
 
+import htmake.jtbot.discord.commands.poll.event.AddOptionButtonEvent;
+import htmake.jtbot.discord.commands.poll.event.AddOptionModalEvent;
 import htmake.jtbot.discord.commands.poll.event.PollButtonEvent;
 import htmake.jtbot.discord.commands.poll.event.PollSlashEvent;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -15,10 +18,16 @@ public class PollCommand extends ListenerAdapter {
 
     private final PollButtonEvent pollButtonEvent;
 
+    private final AddOptionButtonEvent addOptionButtonEvent;
+    private final AddOptionModalEvent addOptionModalEvent;
+
     public PollCommand() {
         this.pollSlashEvent = new PollSlashEvent();
 
         this.pollButtonEvent = new PollButtonEvent();
+
+        this.addOptionButtonEvent = new AddOptionButtonEvent();
+        this.addOptionModalEvent = new AddOptionModalEvent();
     }
 
     @Override
@@ -35,9 +44,27 @@ public class PollCommand extends ListenerAdapter {
         List<String> componentList = List.of(event.getComponentId().split("-"));
 
         if (componentList.get(0).equals("poll")) {
-            int pollId = Integer.parseInt(componentList.get(1));
-            int optionId = Integer.parseInt(componentList.get(2));
-            pollButtonEvent.execute(event, pollId, optionId);
+            if (componentList.get(1).equals("add")) {
+                int pollId = Integer.parseInt(componentList.get(2));
+                addOptionButtonEvent.execute(event, pollId);
+            } else {
+                event.deferEdit().queue();
+                int pollId = Integer.parseInt(componentList.get(1));
+                int optionId = Integer.parseInt(componentList.get(2));
+                pollButtonEvent.execute(event, pollId, optionId);
+            }
+        }
+    }
+
+    @Override
+    public void onModalInteraction(ModalInteractionEvent event) {
+        List<String> componentList = List.of(event.getModalId().split("-"));
+
+        if (componentList.get(0).equals("poll")) {
+            if (componentList.get(1).equals("add")) {
+                int pollId = Integer.parseInt(componentList.get(2));
+                addOptionModalEvent.execute(event, pollId);
+            }
         }
     }
 }
