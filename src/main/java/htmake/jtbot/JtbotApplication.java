cc185d524b54319b1f2a4af5a53dcffdd24fb091
오진서 <s22030@gsm.hs.kr>
2024-06-17 19:59:38 +0900

    add :: EnableScheduling

--- src/main/java/htmake/jtbot/JtbotApplication.java
+++ src/main/java/htmake/jtbot/JtbotApplication.java
@@ -4,9 +4,11 @@ import htmake.jtbot.discord.bot.JtBot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
+@EnableScheduling
 public class JtbotApplication {
 
 	public static void main(String[] args) {
