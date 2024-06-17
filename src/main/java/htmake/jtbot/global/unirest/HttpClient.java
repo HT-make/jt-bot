6ad36dcc62d31d4b43a02685559b88e8635eac5c
오진서 <s22030@gsm.hs.kr>
2024-06-17 20:00:01 +0900

    add :: requests

--- src/main/java/htmake/jtbot/global/unirest/HttpClient.java
+++ src/main/java/htmake/jtbot/global/unirest/HttpClient.java
@@ -2,10 +2,17 @@ package htmake.jtbot.global.unirest;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
+import kotlin.Pair;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface HttpClient {
 
     HttpResponse<JsonNode> sendPostRequest(String endpoint, String requestBody);
+
+    HttpResponse<JsonNode> sendPostRequest(String endPoint, Pair<String, String> routeParam, String requestBody);
+
+    HttpResponse<JsonNode> sendGetRequest(String endPoint, Pair<String, String> routeParam);
+
+    HttpResponse<JsonNode> sendDeleteRequest(String endPoint, Pair<String, String> routeParam);
 }
--- src/main/java/htmake/jtbot/global/unirest/impl/HttpClientImpl.java
+++ src/main/java/htmake/jtbot/global/unirest/impl/HttpClientImpl.java
@@ -6,6 +6,7 @@ import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
+import kotlin.Pair;
 
 public class HttpClientImpl implements HttpClient {
 
@@ -25,6 +26,44 @@ public class HttpClientImpl implements HttpClient {
         } catch (UnirestException e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to send HTTP POST request", e);
+        }
+    }
+
+    @Override
+    public HttpResponse<JsonNode> sendPostRequest(String endPoint, Pair<String, String> routeParam, String requestBody) {
+        try {
+            return Unirest.post(RestServiceType.DEFAULT_URL + endPoint)
+                    .header("Content-Type", "application/json")
+                    .routeParam(routeParam.getFirst(), routeParam.getSecond())
+                    .body(requestBody)
+                    .asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to send HTTP POST request", e);
+        }
+    }
+
+    @Override
+    public HttpResponse<JsonNode> sendGetRequest(String endPoint, Pair<String, String> routeParam) {
+        try {
+            return Unirest.get(RestServiceType.DEFAULT_URL + endPoint)
+                    .routeParam(routeParam.getFirst(), routeParam.getSecond())
+                    .asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to send HTTP GET request", e);
+        }
+    }
+
+    @Override
+    public HttpResponse<JsonNode> sendDeleteRequest(String endPoint, Pair<String, String> routeParam) {
+        try {
+            return Unirest.delete(RestServiceType.DEFAULT_URL + endPoint)
+                    .routeParam(routeParam.getFirst(), routeParam.getSecond())
+                    .asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to send HTTP DELETE request", e);
         }
     }
 }
