044644858c7a22ed113f4777a10c5dd04abd4bc2
오진서 <s22030@gsm.hs.kr>
2024-06-17 20:00:23 +0900

    add :: schedule message command

--- src/main/java/htmake/jtbot/discord/commands/global/GlobalCommand.java
+++ src/main/java/htmake/jtbot/discord/commands/global/GlobalCommand.java
@@ -30,7 +30,9 @@ public class GlobalCommand extends ListenerAdapter {
     public void onReady(@NotNull ReadyEvent event){
         List<CommandData> commandData = List.of(
                 Commands.slash("호떡아", "호떡이에게 질문합니다.").addOptions(insertQuestion()),
-                Commands.slash("투표", "투표를 생성합니다.").addOptions(createPollForm())
+                Commands.slash("투표", "투표를 생성합니다.").addOptions(createPollForm()),
+                Commands.slash("메시지-예약", "메시지를 예약합니다.").addOptions(createScheduledMessageForm()),
+                Commands.slash("메시지-목록", "예약된 메시지 목록을 확인합니다.")
         );
 
         event.getJDA().updateCommands().addCommands(commandData).queue();
@@ -59,5 +61,16 @@ public class GlobalCommand extends ListenerAdapter {
                 .addChoice("아니오", "false");
 
         return List.of(title, optionList, description, duplication);
+    }
+
+    private List<OptionData> createScheduledMessageForm() {
+        OptionData message = new OptionData(OptionType.STRING, "메시지", "예약할 메시지를 입력해주세요!")
+                .setRequired(true)
+                .setRequiredLength(1, 850);
+
+        OptionData scheduledTime = new OptionData(OptionType.STRING, "예약-시간", "[2024-04-22 20:30]과 같은 형식으로 예약할 미래의 시간을 입력해주세요!")
+                .setRequired(true);
+
+        return List.of(message, scheduledTime);
     }
 }
